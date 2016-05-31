8ac6d2050cbcbe8ec28a8c5e356a7d87f421d8e8
Adam Parker <adam@theadamparker.com>
2016-05-31 15:26:59 -0400

    Linked Google Sheet and Untappd for basic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 window.onload = function() { init() };
 
+// global var
+var requestParameters = '?client_id=7665C24D5D9B86BD78146E89DED1A50440A56D43&client_secret=C5F5D761BB5AFFBB02A05F7C23A86AAC157CECE0';
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1kwgYEHQT0BPk_-pSerQDmLQslBwceMN93LXfFSdwQu8/pubhtml';
 
 function init() {
@@ -9,19 +11,37 @@ function init() {
 }
 
 function showInfo(data, tabletop) {
-  visualizeIt(data);
-}
+  console.log(data);
 
-function visualizeIt(data){
   for (var i = 0; i < data.length; i++) {
-    // the loop
-    var LowerLevelLeft = data[i].LowerLevelLeft;
-    var description = data[i].Description;
 
-    $(".services").append('<div class="service">' +
-      '<h2>' + LowerLevelLeft + '</h2>' +
-      '<p>' + description + '</p>' +
-      '</div>'
-    );
+    if (data[i].Active === '0') {
+      // nothing on tap
+      console.log("empty");
+    } else {
+      // function for each beer
+
+      var beerID = data[i].BIN;
+      var url = 'https://api.untappd.com/v4/beer/info/' + beerID + requestParameters;
+
+      $.getJSON(url, function(untappddata) {
+
+        var beerName = untappddata.response.beer.beer_name;
+        var beerBrewery = untappddata.response.beer.brewery.brewery_name;
+        var beerABV = untappddata.response.beer.beer_abv;
+        var beerIBU = untappddata.response.beer.beer_ibu;
+        var beerRating = untappddata.response.beer.rating_score;
+
+        $(".beers").append('<div>' +
+        '<h2>' + beerName + '</h2>' +
+        '<h4>' + beerBrewery + '</h4>' +
+        '<p>' + beerABV + '% ABV, IBU ' + beerIBU + '</p>' +
+        '<p>Rating: ' + beerRating + '</p>' +
+        '</div>'
+        );
+
+      });
+
+    } // end else
   }
 }
