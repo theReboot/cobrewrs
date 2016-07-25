2374c319dbf0752eed21737c85fccb2e881796e9
Adam Parker <adam@theadamparker.com>
2016-07-25 17:41:15 -0400

    Working to add the edit panel

--- js/scripts.js
+++ js/scripts.js
@@ -148,53 +148,6 @@ $("#foo").submit(function(event){
     event.preventDefault();
 });
 
-// adapted from this: http://callmenick.com/post/single-page-site-with-smooth-scrolling-highlighted-link-and-fixed-navigation
-
-$(document).ready(function(){
-  $("nav a").click(function(evn){
-    evn.preventDefault();
-    $('html,body').scrollTo(this.hash, this.hash);
-  });
-
-  /**
-     * This part handles the highlighting functionality.
-     * We use the scroll functionality again, some array creation and
-     * manipulation, class adding and class removing, and conditional testing
-     */
-    var aChildren = $("nav").children(); // find the a children of the list items
-    var aArray = []; // create the empty aArray
-    for (var i=0; i < aChildren.length; i++) {
-        var aChild = aChildren[i];
-        var ahref = $(aChild).attr('href');
-        aArray.push(ahref);
-    } // this for loop fills the aArray with attribute href values
-
-    $(window).scroll(function(){
-        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
-        var windowHeight = $(window).height(); // get the height of the window
-        var docHeight = $(document).height();
-
-        for (var i=0; i < aArray.length; i++) {
-            var theID = aArray[i];
-            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
-            var divHeight = $(theID).height(); // get the height of the div in question
-            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
-                $("a[href='" + theID + "']").addClass("nav-active");
-            } else {
-                $("a[href='" + theID + "']").removeClass("nav-active");
-            }
-        }
-
-        if(windowPos + windowHeight == docHeight) {
-            if (!$("nav li:last-child a").hasClass("nav-active")) {
-                var navActiveCurrent = $(".nav-active").attr("href");
-                $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
-                $("nav li:last-child a").addClass("nav-active");
-            }
-        }
-    });
-});
-
 var searchURL = 'https://api.untappd.com/v4/search/beer' + requestParameters;
 
 $("#searchterm").keyup(function(e){
@@ -221,4 +174,54 @@ $("#searchterm").keyup(function(e){
       );
     });
   });
+});
+
+$(document).ready(function(){
+  // adapted from this: http://callmenick.com/post/single-page-site-with-smooth-scrolling-highlighted-link-and-fixed-navigation
+  $("nav a").click(function(evn){
+    evn.preventDefault();
+    $('html,body').scrollTo(this.hash, this.hash);
+  });
+
+  var aChildren = $("nav").children(); // find the a children of the list items
+  var aArray = []; // create the empty aArray
+  for (var i=0; i < aChildren.length; i++) {
+      var aChild = aChildren[i];
+      var ahref = $(aChild).attr('href');
+      aArray.push(ahref);
+  } // this for loop fills the aArray with attribute href values
+
+  $(window).scroll(function(){
+    var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+    var windowHeight = $(window).height(); // get the height of the window
+    var docHeight = $(document).height();
+
+    for (var i=0; i < aArray.length; i++) {
+      var theID = aArray[i];
+      var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+      var divHeight = $(theID).height(); // get the height of the div in question
+      if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+        $("a[href='" + theID + "']").addClass("nav-active");
+      } else {
+        $("a[href='" + theID + "']").removeClass("nav-active");
+      }
+    }
+
+    if(windowPos + windowHeight == docHeight) {
+      if (!$("nav li:last-child a").hasClass("nav-active")) {
+        var navActiveCurrent = $(".nav-active").attr("href");
+        $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+        $("nav li:last-child a").addClass("nav-active");
+      }
+    }
+  });
+
+  $('.toggle').on('click', function() {
+    $(this).parent('.beerWrap').stop().addClass('activeEdit');
+  });
+
+  $('.close').on('click', function() {
+    $(this).closest('.beerWrap').stop().removeClass('activeEdit');
+  });
+
 });
